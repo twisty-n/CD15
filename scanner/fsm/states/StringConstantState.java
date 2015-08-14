a98dfefe55e390b8db7c398ecc77a5546b11470e
Tristan Newmann <tristan_dev@tristans-mbp.newcastle.edu.au>
2015-08-14 10:32:07 +1000

    may be able to handle string literals, haven't tested it out fully yet

---
+++ scanner/fsm/states/StringConstantState.java
@@ -0,0 +1,13 @@
+package scanner.fsm.states;
+
+/**
+ * Author:          Tristan Newmann
+ * Student Number:  c3163181
+ * Email:           c3163181@uon.edu.au
+ * Date Created:    14/08/15
+ * File Name:       StringConstantState
+ * Project Name:    CD15
+ * Description:
+ */
+public class StringConstantState {
+}
